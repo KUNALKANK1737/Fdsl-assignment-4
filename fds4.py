94ba7e64f55e08cc69f1c586e0ba8d5028adceae
kunal kank <112045799+KUNALKANK1737@users.noreply.github.com>
2022-10-14 08:57:16 +0530

    Add files via upload

---
+++ fds4.py
@@ -0,0 +1,54 @@
+
+def selection_sort():
+    marks=[]
+    n=int(input("Enter the number of elememts to be taken :"))
+    for i in range (n):
+        r=int(input("Enter the value to be inserted :"))
+        marks.append(r)
+    print(marks)
+    
+    for i in range (len(marks)):
+       min_m=i
+       for j in range (i+1,len(marks)):
+          if(marks[min_m]>marks[j]):
+            temp=marks[min_m]
+            marks[min_m]=marks[j]
+            marks[j]=temp
+    print("The sorted list is :")
+    print(marks)
+    if(n>=5):
+       print("The top five members are :")
+       for i in range (5):
+           print(marks[i])
+def bubble_sort():
+    marks=[]
+    n=int(input("Enter the number of elememts to be taken :"))
+    for i in range (n):
+        r=int(input("Enter the value to be inserted :"))
+        marks.append(r)
+    print(marks)
+    for i in range (n-1):
+        
+        for j in range (0,n-i-1):
+            if(marks[j]>marks[j+1]):
+                temp=marks[j]
+                marks[j]=marks[j+1]
+                marks[j+1]=temp
+    print("The sorted list is :")
+    print(marks)
+    if(n>=5):
+       print("The top five members are :")
+       for i in range (5):
+        print(marks[i])
+               
+        
+while(True):
+    print("press 1:selection sort\npress 2:bubble sort")
+    d=int(input("Enter your choice :"))
+    if(d==1):
+     selection_sort()
+    if(d==2):
+     bubble_sort()
+
+
+
